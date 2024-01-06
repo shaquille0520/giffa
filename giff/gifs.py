f9f3a23db2fd2f9de4d280b25017ba0c2ac48225
shaquille0520 <31034222+shaquille0520@users.noreply.github.com>
2024-01-06 00:59:05 +0000

    Add files via upload

---
+++ giff/gifs.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root = Tk()
+
+frameCnt = 13
+frames = [PhotoImage(file='C:\\Users\\mui\\Desktop\\giff\\208803700.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+
+def update(ind):
+
+    frame = frames[ind]
+    ind += 1
+    if ind == frameCnt:
+        ind = 0
+    label.configure(image=frame)
+    root.after(100, update, ind)
+label = Label(root)
+label.pack()
+root.after(0, update, 0)
+root.mainloop()
